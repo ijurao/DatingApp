1e887a5b21dd95f71169b4d83e44c4d5e1aedc73
ijurao ijurao <ijurao@gmail.com>
2020-05-16 18:54:06 +1000

    Initial

---
+++ DatingApp/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+  
+    public class ValuesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public ValuesController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Values.ToListAsync());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var v = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            if(v != null)
+              return Ok(v);
+
+            return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class ApplicationDbContext:DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(new Value { Id = 1,Name = "Value101" });
+            modelBuilder.Entity<Value>().HasData(new Value { Id = 2,Name = "Value102" });
+            modelBuilder.Entity<Value>().HasData(new Value { Id = 3,Name = "Value103" });
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
